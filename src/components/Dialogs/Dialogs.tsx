5c0594248f7e9bcc16b8d845974b9a636c393ab3
nikolasmushroom <dashagribok0905@gmail.com>
2024-08-26 14:37:16 +0300

    Adding Dialogs.tsx and Dialogs.module.css

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Dialogs.module.css";
+
+export const Dialogs : React.FC = () => {
+    return (
+        <div className={classes.content}>
+            Dialogs
+        </div>
+    )
+}

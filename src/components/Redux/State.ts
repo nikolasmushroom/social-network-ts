f5021ea64a94a9f7d6dcc17f03ebb72afe61ab51
nikolasmushroom <dashagribok0905@gmail.com>
2024-09-05 23:56:00 +0300

    Making object store

--- src/components/Redux/State.ts
+++ src/components/Redux/State.ts
@@ -1,5 +1,5 @@
 import {v1} from "uuid";
-let store = {
+export const store = {
     _state :  {
     dialogsPage: <DialogsPageType> {
         dialogs : <DialogType[]> [
@@ -30,7 +30,25 @@ let store = {
 },
     rerenderEntireTree ()  {
         console.log('State changed')
+    },
+    addPost  () {
+        const newPost : PostType = {
+            id: v1(),
+            message : store._state.profilePage.inputValue,
+            likesCount : 0,
+        }
+        store._state.profilePage.posts.push(newPost)
+        store._state.profilePage.inputValue = '';
+        store.rerenderEntireTree()
+    },
+    changeInput  (newInput : string) {
+        store._state.profilePage.inputValue = newInput
+        store.rerenderEntireTree()
+    },
+    subscribe (observer : () => void) {
+        store.rerenderEntireTree = observer
     }
+
 }
 export type DialogType = {
     id: string
@@ -74,55 +92,11 @@ export type RootStateType = {
     navigationPage: NavigationType
 }
 
-let rerenderEntireTree = () => {
-    console.log('State changed')
-}
 
-export const state : RootStateType = {
-    dialogsPage: <DialogsPageType> {
-        dialogs : <DialogType[]> [
-            {id: v1(), image: 'https://d2q9lphzn5ioni.cloudfront.net/uploads/2024/02/ai-generated-people-beautiful-girl.webp', name: 'Hannah'},
-            {id: v1(), image: 'https://cdnstorage.sendbig.com/unreal/female.webp', name: 'Jane'},
-            {id: v1(), image: 'https://live.staticflickr.com/65535/50999002523_08e4353b95.jpg', name: 'Vlad'},
-            {id: v1(), image: 'https://images.unsplash.com/photo-1712847333453-740d9665aa5d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: 'Farhad'},
-        ],
-        messages: <MessageType[]>[
-            {id: v1(), message: 'Hi'},
-            {id: v1(), message: 'How is your day?'},
-            {id: v1(), message: 'How are you?'},
-            {id: v1(), message: 'Whats wrong with you?'},
-        ]
-    },
-    profilePage: {
-        posts: <PostType[]> [
-            {id : v1(),  message : 'Hello, its me', likesCount: 12},
-            {id : v1(),  message : 'My favorite color is red', likesCount: 5},
-            {id : v1(),  message : 'Hello, its me', likesCount: 1},
-            {id : v1(),  message : 'Hello, its me', likesCount: 2},
-        ],
-        inputValue : '',
-    },
-    navigationPage: <NavigationType>{
-
+declare global {
+    interface Window {
+        store: typeof store;
     }
 }
-export const addPost = () => {
-    const newPost : PostType = {
-        id: v1(),
-        message : state.profilePage.inputValue,
-        likesCount : 0,
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.inputValue = '';
-    rerenderEntireTree()
-}
 
-export const changeInput = (newInput : string) => {
-    state.profilePage.inputValue = newInput
-    rerenderEntireTree()
-}
-
-
-export const subscribe = (observer : () => void) => {
-    rerenderEntireTree = observer
-}
+window.store = store

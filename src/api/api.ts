885e7099bf4a6e33505882557db7663f54e0f828
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-04 00:49:38 +0300

    Creating usersAPI object and adding in methods like getUsers for more readable and useful structure.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 
-export const getUsers = (currentPage: number, pageSize: number) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        {
-            withCredentials: true
-        })
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        'API-KEY': ''
+    }
+})
+export const usersAPI  = {
+    getUsers(currentPage: number, pageSize: number) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
 }

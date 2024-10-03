48a583f86a5168f113025bb9d626fe31c73b3065
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-04 01:28:29 +0300

    Adding to usersAPI new method changeFollowStatus which returning new data with follow and unfollow information.

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,11 +7,22 @@ const instance = axios.create({
         'API-KEY': ''
     }
 })
-export const usersAPI  = {
+export const usersAPI = {
     getUsers(currentPage: number, pageSize: number) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data
             })
+    },
+    changeFollowStatus(userId: string, followStatus : boolean) {
+        return followStatus
+            ? instance.post(`follow/${userId}`)
+                .then(response => {
+                    return response.data
+                })
+            : instance.delete(`follow/${userId}`)
+                .then(response => {
+                    return response.data
+                })
     }
 }

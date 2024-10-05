754f05b9f1af72a83f62917a39b93f5dc14b06cc
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-05 21:40:09 +0300

    Object mapDispatchToProps was refactored to object which transit toMyPostsContainer alike mapDispatchToProps. Was renamed action creators addPost and changeInput.

--- src/components/Profile/MyPosts/MyPostContainer.tsx
+++ src/components/Profile/MyPosts/MyPostContainer.tsx
@@ -1,22 +1,11 @@
 import {RootReduxStateType} from "../../Redux/redux-store";
 import {MyPosts} from "./MyPosts";
-import {addPostActionCreator, changeInputActionCreator} from "../../Redux/profile-reducer";
+import {addPost, changeInput} from "../../Redux/profile-reducer";
 import {connect} from "react-redux";
-import {ActionTypes} from "../../Redux/store";
 const mapStateToProps = (state: RootReduxStateType) => {
     return {
         posts : state.profilePage.posts,
         inputValue : state.profilePage.inputValue,
     }
 }
-const mapDispatchToProps = (dispatch : (action : ActionTypes) => void) => {
-    return {
-        onChangeInput : (text : string) => {
-            dispatch(changeInputActionCreator(text))
-        },
-        addNewPost : (inputValue : string) => {
-            dispatch(addPostActionCreator(inputValue))
-        }
-    }
-}
-export const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+export const MyPostsContainer = connect(mapStateToProps, {changeInput, addPost})(MyPosts);

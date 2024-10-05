c6e4a14bbc90a3ccad2ada01a05002d7a9af7007
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-05 21:36:27 +0300

    Was modified component Button.tsx and added children props.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,10 +1,13 @@
 import {ComponentPropsWithoutRef} from "react";
 
-export const Button = ({disabled, title, onClick, className, ...restProps}: ComponentPropsWithoutRef<'button'>) => {
+export const Button = ({disabled, title, onClick, className, children, ...restProps}: ComponentPropsWithoutRef<'button'>) => {
     return <button
         style={{pointerEvents: disabled ? 'none' : 'all'}}
         className={className}
         onClick={onClick}
         disabled={disabled}
-        {...restProps}/>
+        {...restProps}
+    >
+        {children}
+    </button>
 }

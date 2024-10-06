914090192c67e9b5e9a9ee7304dc968bd9a7b171
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-06 20:06:32 +0300

    Was changed method getUser of UsersContainer, and encapsulated in getUsers, using the thunk from 'react-thunk'. Also was deleted useless callback setCurrentPage.

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -4,16 +4,14 @@ import {RootReduxStateType} from "../Redux/redux-store";
 import {UserType} from "../Redux/store";
 import styles from './Users.module.css'
 import {
+    getUsers,
     setCurrentPage,
     setMaxCount,
-    setTotalUsersCount,
-    setUsers,
     toggleFollow,
-    toggleIsFetching, toggleIsFollowingProgress,
+    toggleIsFollowingProgress,
 } from "../Redux/users-reducer";
 import Users from "./Users";
 import {Preloader} from "../common/Preloader";
-import {usersAPI} from "../../api/api";
 
 export type UsersClassPropsType = {
     users: UserType[]
@@ -22,44 +20,26 @@ export type UsersClassPropsType = {
     currentPage: number
     maxCount: number
     toggleFollow: (userId: string) => void
-    setUsers: (users: UserType[]) => void
     setCurrentPage: (page: number) => void
-    setTotalUsersCount: (totalUsersCount: number) => void
     setMaxCount: (newCount: number) => void
     isFetching: boolean
-    toggleIsFetching: (isFetching: boolean) => void
     toggleIsFollowingProgress: (isFollowing : boolean, userId: string) => void
-    followingInProgress: Array<string>
+    followingInProgress: Array<string>,
+    getUsers: (currentPage : number, pageSize : number) => void
 }
 
 class UsersContainer extends React.Component<UsersClassPropsType> {
-    getUser(){
-        this.props.toggleIsFetching(true)
-        usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
-            .then(data => {
-                this.props.setUsers(data.items)
-                this.props.setTotalUsersCount(data.totalCount)
-            })
-            .finally(() => {
-                this.props.toggleIsFetching(false);
-            })
-    }
     componentDidMount = () => {
-            if (!this.props.users.length) {
-                this.getUser()
-            }
+            this.props.getUsers(this.props.currentPage, this.props.pageSize);
     }
 
     componentDidUpdate(prevProps: Readonly<UsersClassPropsType>) {
         if (prevProps.currentPage !== this.props.currentPage) {
-            this.getUser()
+            this.props.getUsers(this.props.currentPage, this.props.pageSize);
         }
     }
 
     render() {
-        const setCurrentPage = (page: number) => {
-            this.props.setCurrentPage(page);
-        }
         return (
             <>
                 {this.props.isFetching &&
@@ -67,16 +47,13 @@ class UsersContainer extends React.Component<UsersClassPropsType> {
                 }
                 {!this.props.isFetching &&
                     <Users users={this.props.users}
-                           setUsers={this.props.setUsers}
                            totalUsersCount={this.props.totalUsersCount}
-                           setTotalUsersCount={this.props.setTotalUsersCount}
                            maxCount={this.props.maxCount}
                            setMaxCount={this.props.setMaxCount}
                            currentPage={this.props.currentPage}
                            setCurrentPage={this.props.setCurrentPage}
                            pageSize={this.props.pageSize}
                            toggleFollow={this.props.toggleFollow}
-                           getUsersFromAPI={setCurrentPage}
                            toggleIsFollowingProgress={this.props.toggleIsFollowingProgress}
                            followingInProgress={this.props.followingInProgress}
                     />
@@ -101,10 +78,8 @@ const mapStateToProps = (state: RootReduxStateType) => {
 }
 export default connect(mapStateToProps, {
     toggleFollow,
-    setUsers,
     setCurrentPage,
-    setTotalUsersCount,
     setMaxCount,
-    toggleIsFetching,
     toggleIsFollowingProgress,
+    getUsers,
 })(UsersContainer);

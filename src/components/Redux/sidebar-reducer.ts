01d519a0cb21751aedd3dd2e6da7d4a719cea67b
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-06 22:38:14 +0300

    Was added type, which we have to return from sidebar-reducer.ts

--- src/components/Redux/sidebar-reducer.ts
+++ src/components/Redux/sidebar-reducer.ts
@@ -1,5 +1,5 @@
 import {ActionTypes, NavigationType} from "./store";
 const initialState : NavigationType = {}
-export const sidebarReducer = (state = initialState, action : ActionTypes) => {
+export const sidebarReducer = (state = initialState, action : ActionTypes) : NavigationType => {
     return state
 }

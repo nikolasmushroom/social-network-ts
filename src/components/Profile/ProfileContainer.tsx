bce571bad7217ee38ad4df58df02945a61fd4000
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-08 23:47:55 +0300

    Was added using of AuthRedirectComponent in ProfileContainer.tsx. Also was deleted this logic from render. Was added auth-check on 29 in getUserProfile.

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -5,53 +5,46 @@ import {connect} from "react-redux";
 import {getUserProfile, setUserProfileActionCreator} from "../Redux/profile-reducer";
 import {RootReduxStateType} from "../Redux/redux-store";
 import {ProfileType} from "../Redux/store";
-import {withRouter} from "../Redux/WithRouter";
-import {Navigate} from "react-router-dom";
-import {Preloader} from "../common/Preloader";
+import {withRouter} from "../HOC/WithRouter";
+import {withAuthRedirect} from "../HOC/WithAuthRedirect";
 
 export type ProfileContainerPropsType = {
     setUserProfileActionCreator: (profile: ProfileType) => void
-    getUserProfile: (userId: string) => void
-    isAuth : boolean,
+    getUserProfile: (userId: string, isAuth : boolean) => void
+    isAuth: boolean,
     isLoading: boolean
     profile?: ProfileType
-    params?: {[key: string]: string}
+    params?: { [key: string]: string }
 }
 
 class ProfileContainer extends React.Component <ProfileContainerPropsType> {
+
     componentDidMount() {
-        let userId =  this.props.params?.userId
-        if(!userId){
+        let userId = this.props.params?.userId
+        if (!userId) {
             userId = '31705'
         }
-        this.props.getUserProfile(userId)
+
+        this.props.getUserProfile(userId, this.props.isAuth);
     }
 
 
     render() {
-        if(this.props.isLoading){
-            return <Preloader className={classes.preloader}/>
-        }
-        if (!this.props.isAuth){
-            return <Navigate to={'/login'}/>
-        }
         return (
             <div className={classes.content}>
                 <Profile profile={this.props.profile}/>
             </div>
         )
     }
-
 }
 
 const mapStateToProps = (state: RootReduxStateType) => {
     return {
         profile: state.profilePage.profile,
-        isAuth: state.auth.isAuth,
-        isLoading: state.auth.isLoading
     }
 }
-export const ProfileWithRouter = withRouter(ProfileContainer)
+const AuthRedirectComponent = withAuthRedirect(ProfileContainer)
+export const ProfileWithRouter = withRouter(AuthRedirectComponent)
 export default connect(mapStateToProps, {
     setUserProfileActionCreator,
     getUserProfile

e68b2e451e9a3dfd763c25183c6d6d1577fadc28
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-08 14:22:30 +0300

    Was added a container class-component LoginContainer.tsx, which map from state to Login.tsx props,with authorization information.

---
+++ src/components/Login/LofinContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {RootReduxStateType} from "../Redux/redux-store";
+import {connect} from "react-redux";
+import {Login} from "./Login";
+
+export type LoginContainerPropsType = {
+    getLogInfo : () => void
+    isAuth : boolean
+}
+export class LoginContainer extends React.Component<LoginContainerPropsType>{
+    componentDidMount() {
+        if(this.props.getLogInfo)
+            this.props.getLogInfo()
+    }
+
+    render(){
+        return (
+            <Login isAuth={this.props.isAuth} getLogInfo={this.props.getLogInfo}/>
+        )
+    }
+
+
+}
+const mapStateToProps = (state : RootReduxStateType) => {
+    return {
+        isAuth : state.auth.isAuth,
+    }
+}
+export default connect(mapStateToProps, {})(Login);

1a2f1192edf47d63bfc23dd24831ec3bfdb76563
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-09 23:27:40 +0300

    Was added right updated imports and was updated path (Login to AuthContainer).

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import './App.css';
-import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import {Login} from "./components/Login/Login";
+import {AuthContainer} from "./components/Login/LoginContainer";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 const App = () => {
 
@@ -35,7 +35,7 @@ const App = () => {
                             <Route path={'/news'} element={<News/>}/>
                             <Route path={'/music'} element={<Music/>}/>
                             <Route path={'/settings'} element={<Settings/>}/>
-                            <Route path={'/login'} element={<Login/>}/>
+                            <Route path={'/login'} element={<AuthContainer/>}/>
                         </Routes>
                     </div>
                 </div>

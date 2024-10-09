bc7d409f0ca88ea45e8f8c1a46fc6c2cb3e4c312
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-09 23:23:27 +0300

    Was debugged error with not right destructurisation of isAuth in  LoginContainerPropsType.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,10 +4,12 @@ export type LoginContainerPropsType = {
     getLogInfo? : () => void
     isAuth? : boolean
 }
-export const Login = (isAuth : LoginContainerPropsType) => {
+export const Login = ({isAuth} : LoginContainerPropsType) => {
+
     if(isAuth){
         return <Navigate to={'/'}/>
     }
+
     return (
         <div>LOGIN</div>
     )

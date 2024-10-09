f40f372f935600a34d5cbacc691924c3d6135a5b
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-09 20:48:59 +0300

    Was added types for component, which wrapped by WithAuthRedirect.tsx

--- src/components/HOC/WithAuthRedirect.tsx
+++ src/components/HOC/WithAuthRedirect.tsx
@@ -1,15 +1,14 @@
-import React, {ComponentType} from "react";
+import React, {ComponentProps, ComponentType} from "react";
 import {Preloader} from "../common/Preloader";
 import {Navigate} from "react-router-dom";
 import {RootReduxStateType} from "../Redux/redux-store";
 import {connect} from "react-redux";
-// type withAuthRedirectProps = {
-//     isAuth: boolean;
-//     isLoading: boolean;
-// }
-
-export const withAuthRedirect = <P extends object>(Component : ComponentType<P>) => {
-    const RedirectComponent  = (props : any)   => {
+type withAuthRedirectProps = {
+    isAuth: boolean;
+    isLoading: boolean;
+}
+export const withAuthRedirect = <P extends ComponentProps<ComponentType>>(Component : ComponentType<P>) => {
+    const RedirectComponent : any  = (props: P & withAuthRedirectProps)   => {
         if(props.isLoading){
             return <Preloader/>
         }
@@ -18,7 +17,7 @@ export const withAuthRedirect = <P extends object>(Component : ComponentType<P>)
         }
         return <Component {...props}/>
     };
-    const mapStateToRedirectProps = (state: RootReduxStateType) => ({
+    const mapStateToRedirectProps = (state: RootReduxStateType): withAuthRedirectProps  => ({
         isAuth: state.auth.isAuth,
         isLoading: state.auth.isLoading
     })

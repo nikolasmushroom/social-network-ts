d4f0bcc819248eda2d631a8943ff4de1d82efa60
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-13 22:23:34 +0300

    Was added changing of isAuth in state in setUserDataAC. Was added error state. Were added redux-thunks login and logout. Was added setError action creator, which change error in state with error from API.

--- src/components/Redux/auth-reducer.ts
+++ src/components/Redux/auth-reducer.ts
@@ -4,40 +4,56 @@ import {authAPI} from "../../api/api";
 export const SET_USER_DATA = 'SET_USER_DATA'
 export const SET_AUTH_ME = 'SET_AUTH_ME'
 export const SET_LOADING = 'SET_LOADING'
-export const initialState : HeaderStateType = {
-    userId : 0,
-    email : '',
+export const initialState: HeaderStateType = {
+    userId: 0,
+    email: '',
     login: '',
     isAuth: false,
-    isLoading: false
+    isLoading: false,
+    error: ''
 }
-export const authReducer = (state = initialState, action : ActionTypes) : HeaderStateType => {
+export const authReducer = (state = initialState, action: ActionTypes): HeaderStateType => {
     switch (action.type) {
         case 'SET_USER_DATA':
-            return {...state, userId : action.data.userId, email : action.data.email, login : action.data.login, isAuth : true}
+            return {
+                ...state,
+                userId: action.data.userId,
+                email: action.data.email,
+                login: action.data.login,
+                isAuth: action.data.isAuth,
+            }
         case 'SET_AUTH_ME' :
-            return {...state, isAuth : true}
+            return {...state, isAuth: true}
         case 'SET_LOADING' :
             return {...state, isLoading: action.isLoading}
+        case 'SET_ERROR' :
+            return {...state, error: action.error}
         default :
             return state
     }
 }
-export const setUserDataAC = (userId: number, email : string, login : string) : setUserDataACType => {
+export const setUserDataAC = (userId: number, email: string, login: string, isAuth: boolean): setUserDataACType => {
     return {
-        type : 'SET_USER_DATA',
-        data : {userId, email, login}}
+        type: 'SET_USER_DATA',
+        data: {userId, email, login, isAuth}
+    }
 }
-export const setAuthMe = () : setAuthMeType => {
+export const setAuthMe = (): setAuthMeType => {
     return {
         type: 'SET_AUTH_ME',
     }
 }
-export const setLoading = (isLoading : boolean) : setLoadingType => {
+export const setLoading = (isLoading: boolean): setLoadingType => {
     return {
         type: 'SET_LOADING',
         isLoading: isLoading
     }
+}
+export const setError = (error : string) => {
+    return {
+        type : 'SET_ERROR',
+        error : error
+    } as const
 }
 export const getLogInfo = () => {
     return (dispatch: any) => {
@@ -46,7 +62,7 @@ export const getLogInfo = () => {
             .then(data => {
                 if (data.resultCode === 0) {
                     dispatch(setAuthMe())
-                    dispatch(setUserDataAC(data.data.id, data.data.email, data.data.login)); // Пользователь авторизован
+                    dispatch(setUserDataAC(data.data.id, data.data.email, data.data.login, true))
                 }
             })
             .finally(() => {
@@ -54,3 +70,25 @@ export const getLogInfo = () => {
             })
     };
 };
+export const login = (email: string, password: string, rememberMe: boolean = false) => {
+    return (dispatch: any) => {
+        authAPI.login(email, password, rememberMe)
+            .then(data => {
+                if (data.resultCode === 0) {
+                    dispatch(getLogInfo())
+                }else{
+                    dispatch(setError(data.messages))
+                }
+            })
+    }
+}
+export const logout = () => {
+    return (dispatch: any) => {
+        authAPI.logout()
+            .then(data => {
+                if (data.resultCode === 0) {
+                    dispatch(setUserDataAC(0, '', '', false))
+                }
+            })
+    }
+}

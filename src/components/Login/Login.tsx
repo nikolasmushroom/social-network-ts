96eb8b402b2f23dcfdfde9262499cbdce3e47756
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-13 22:36:34 +0300

    Was added useEffect to controle isAuth changing. Was added LoginForm.tsx to Login.tsx.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,17 +1,24 @@
 import {Navigate} from "react-router-dom";
-import React from "react";
-export type LoginContainerPropsType = {
-    getLogInfo? : () => void
-    isAuth? : boolean
+import React, {useEffect} from "react";
+import classes from "./Login.module.css";
+import {LoginForm} from "./LoginForm";
+
+export type LoginPropsType = {
+    login: (email: string, password: string, rememberMe: boolean) => void
+    isAuth: boolean
+    error: string
 }
-export const Login = ({isAuth} : LoginContainerPropsType) => {
-
-    if(isAuth){
-        return <Navigate to={'/'}/>
+export const Login = ({isAuth, login, error}: LoginPropsType) => {
+    useEffect(() => {
+    }, [isAuth])
+    if (isAuth) {
+        return <Navigate to={'/profile'}/>
+    } else {
+        return (
+            <div className={classes.login}>
+                <h1>LOGIN</h1>
+                <LoginForm login={login} error={error}/>
+            </div>
+        )
     }
-
-    return (
-        <div>LOGIN</div>
-    )
-
 }

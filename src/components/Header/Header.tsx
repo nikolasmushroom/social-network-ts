99a414b4522f7efc1749ed5de9681fb108434950
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-13 22:33:27 +0300

    Was added logout Button.tsx component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import classes from './Header.module.css';
 import logo from  '../../asserts/images/logo.png'
 import {NavLink} from "react-router-dom";
+import {Button} from "../common/Button";
 type HeaderPropsType = {
     isAuth : boolean
-    login  :string
+    login  : string
+    logout? : () => void
 }
-const Header = ({isAuth, login} : HeaderPropsType) => {
+const Header = ({isAuth, login, logout} : HeaderPropsType) => {
     return (
         <header className={classes.header}>
             <img src={logo} alt="logo"/>
             <div className={classes.loginBlock}>
-                {isAuth ? <div>{login}</div> : <NavLink to={'/login'}>Login</NavLink>}
+                {isAuth ? <div>{login} <Button onClick={logout}>Log out</Button> </div>: <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>
     )

1659a39af772d4ff12c4e5208aa109d3ddde8210
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-19 15:48:17 +0300

    In ProfileInfo.tsx was added avatar-image check.

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,6 +4,7 @@ import background from '../../../asserts/images/background-profile.jpg'
 import {Preloader} from "../../common/Preloader";
 import {ProfilePropsType} from "../Profile";
 import {ProfileStatus} from "../ProfileStatus";
+import avatar from './../../../asserts/avatars/defaultUserImage.png';
 
 const ProfileInfo = ({profile, status, updateUserStatus} : ProfilePropsType) => {
     if(!profile){
@@ -17,7 +18,7 @@ const ProfileInfo = ({profile, status, updateUserStatus} : ProfilePropsType) => 
                     alt="background"/>
             </div>
             <div className={classes.descriptionBlock}>
-                <img className={classes.avatarImage} src={profile.photos.large} alt="avatar"/>
+                <img className={classes.avatarImage} src={profile.photos.large || profile.photos.small ? profile.photos.large || profile.photos.small : avatar} alt="avatar"/>
                 <div className={classes.about}>
                     <div className={classes.fullNameAndAboutMe}>
                         <div className={classes.fullName}>{profile.fullName}</div>

ea421ffe069c14a6f06b0fbfbc54c76ed6fe257b
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-24 22:10:47 +0300

    Was refactored and relocated App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {Preloader} from "../common/components/Preloader/Preloader";
+import {BrowserRouter} from 'react-router-dom';
+import {MainPage} from "../common/components/MainPage/MainPage/MainPage";
+
+
+type AppPropsType = {
+    initializeApp: () => void
+    initialized: boolean
+}
+
+class App extends React.Component<AppPropsType> {
+    componentDidMount() {
+        this.props.initializeApp()
+    }
+
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
+            <BrowserRouter>
+                <MainPage/>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App

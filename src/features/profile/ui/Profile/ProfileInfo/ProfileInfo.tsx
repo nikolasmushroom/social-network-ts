063798220819d719e2d7a8db632892fdb6ed7bef
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-28 23:35:18 +0300

    In ProfileInfo.tsx was edded local state editMode.

--- src/features/profile/ui/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/features/profile/ui/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from './ProfileInfo.module.css';
 import background from '../../../../../asserts/images/background-profile.jpg'
 import {Preloader} from "../../../../../common/components/Preloader/Preloader";
 import {ProfilePropsType} from "../Profile";
 import avatar from '../../../../../asserts/avatars/defaultUserImage.png';
-import {ProfileStatus} from "./ProfileStatus/ProfileStatus";
+import {Button} from "../../../../../common/components/Button/Button";
+import {ProfileData, } from "./ProfileData/ProfileData";
+import {ProfileDataForm} from "./ProfileData/ProfileDataForm";
 
-const ProfileInfo = ({profile, status, updateUserStatus, isOwner, savePhoto}: ProfilePropsType) => {
+const ProfileInfo = ({profile, status, updateUserStatus, isOwner, savePhoto, updateUserProfile}: ProfilePropsType) => {
+    const [editMode, setEditMode] = useState(false)
     if (!profile) {
         return <Preloader/>
     }
@@ -14,6 +17,9 @@ const ProfileInfo = ({profile, status, updateUserStatus, isOwner, savePhoto}: Pr
         if (e.currentTarget.files) {
             savePhoto(e.currentTarget.files[0]);
         }
+    }
+    const switchEditMode = () => {
+        setEditMode(!editMode)
     }
     return (
         <div className={classes.content}>
@@ -26,24 +32,17 @@ const ProfileInfo = ({profile, status, updateUserStatus, isOwner, savePhoto}: Pr
                 <div className={classes.imageContainer}>
                     <img className={classes.avatarImage} src={profile.photos.large ? profile.photos.large : avatar}
                          alt="avatar"/>
-                    {isOwner && <div>
-                        <label className={classes.fileInputLabel} htmlFor="file-upload">Add new</label>
+                    {isOwner && <div className={classes.fileInputContainer}>
+                        <Button className={classes.fileInputLabel}>Add photo</Button>
                         <input type={'file'} onChange={(event) => onMainPhotoSelected(event)}
                                className={classes.changePhoto}/>
                     </div>
                     }
                 </div>
-                <div className={classes.about}>
-                    <div className={classes.fullNameAndAboutMe}>
-                        <div className={classes.fullName}>{profile.fullName}</div>
-                        <ProfileStatus className={classes.aboutMe} status={status} updateUserStatus={updateUserStatus}/>
-                    </div>
-                    <div className={classes.jobStatus}>
-                        <div>Job status : {profile.lookingForAJob ? 'search' : 'employed'}</div>
-                        <div>Description : {profile.lookingForAJobDescription}</div>
-                    </div>
-                </div>
+                {!editMode ? <ProfileData updateUserProfile={updateUserProfile} switchEditMode={switchEditMode} isOwner={isOwner} profile={profile} status={status} updateUserStatus={updateUserStatus}/> : <ProfileDataForm switchEditMode={switchEditMode} profile={profile} status={status} updateUserStatus={updateUserStatus} updateUserProfile={updateUserProfile}/>}
+
             </div>
+
         </div>
     )
 }

869b07970bd8a3ca3aad9eb88a751f4456e886d7
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-29 23:32:03 +0300

    Were added isOwner, savePhoto, updateUserProfile to type ProfilePropsType

--- src/features/profile/ui/Profile/Profile.tsx
+++ src/features/profile/ui/Profile/Profile.tsx
@@ -4,15 +4,17 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import {MyPostsContainer} from "./MyPosts/MyPostContainer";
 import {ProfileType} from "../../../../app/store/store";
 export type ProfilePropsType = {
+    isOwner : boolean
     profile : ProfileType
     status : string
     updateUserStatus : (status: string) => void
-
+    savePhoto: (file: any) => void
+    updateUserProfile: (profile: ProfileType, setError? : any) => void
 }
-const  Profile= React.memo(({profile, status, updateUserStatus} : ProfilePropsType) => {
+const  Profile= React.memo(({updateUserProfile, isOwner, profile, status, updateUserStatus, savePhoto} : ProfilePropsType) => {
     return (
         <div className={classes.content}>
-            <ProfileInfo profile={profile} status={status} updateUserStatus={updateUserStatus}/>
+            <ProfileInfo updateUserProfile={updateUserProfile} isOwner={isOwner} profile={profile} status={status} updateUserStatus={updateUserStatus} savePhoto={savePhoto}/>
             <MyPostsContainer/>
         </div>
     )

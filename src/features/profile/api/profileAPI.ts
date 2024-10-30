ca5526a2a78c81128abc6962bbdef5686fa96771
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-30 23:01:39 +0300

    Were added types for all async functions.

--- src/features/profile/api/profileAPI.ts
+++ src/features/profile/api/profileAPI.ts
@@ -1,23 +1,34 @@
 import {baseURL} from "../../../api/api";
 import {ProfileType} from "../../../app/store/store";
 
+type ApiResponse<D = {}> = {
+    resultCode: 0 | 1
+    messages: Array<String>,
+    data: D
+}
+type PhotoData = {
+    photos?: {
+        small?: string
+        large?: string
+    }
+}
 export const profileAPI = {
     async getUserProfile(userId: string) {
-        return await baseURL.get(`profile/${userId}`)
+        return await baseURL.get<ProfileType>(`profile/${userId}`)
     },
-    async updateUserProfileData(profile : ProfileType){
-      return await baseURL.put('profile', profile)
+    async updateUserProfileData(profile: ProfileType) {
+        return await baseURL.put<ApiResponse<PhotoData>>('profile', profile)
     },
     async getUserStatus(userId: string) {
-        return await baseURL.get(`profile/status/${userId}`)
+        return await baseURL.get<string>(`profile/status/${userId}`)
     },
     async updateUserStatus(status: string) {
-        return await baseURL.put(`profile/status`, {status: status});
+        return await baseURL.put<ApiResponse>(`profile/status`, {status: status});
     },
     async savePhoto(file: any) {
         const formData = new FormData()
         formData.append('image', file)
-        return await baseURL.put(`profile/photo`, formData, {
+        return await baseURL.put<ApiResponse<PhotoData>>(`profile/photo`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }

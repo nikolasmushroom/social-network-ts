55e6c1d4b6950e322dcb1c9f6569d53f39647bae
nikolasmushroom <dashagribok0905@gmail.com>
2024-10-30 23:00:52 +0300

    Were added type for getUsers return value.

--- src/features/users/api/usersAPI.ts
+++ src/features/users/api/usersAPI.ts
@@ -1,10 +1,14 @@
-// разбить по разным сущностям
 import {baseURL} from "../../../api/api";
 import {profileAPI} from "../../profile/api/profileAPI";
-
+import {UserType} from "../../../app/store/store";
+type ApiResponse = {
+    error : null | string
+    items : Array<UserType>
+    totalCount : number
+}
 export const usersAPI = {
     async getUsers(currentPage: number, pageSize: number) {
-        return await baseURL.get(`users?page=${currentPage}&count=${pageSize}`);
+        return await baseURL.get<ApiResponse>(`users?page=${currentPage}&count=${pageSize}`);
     },
     async changeFollowStatus(userId: string, followStatus: boolean) {
         return followStatus

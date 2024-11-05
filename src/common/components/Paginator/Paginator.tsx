561373cee09d65b63f0a2fc67258f91e625fc7c6
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-05 23:27:10 +0300

    Was changed Paginator.tsx

--- src/common/components/Paginator/Paginator.tsx
+++ src/common/components/Paginator/Paginator.tsx
@@ -1,13 +1,15 @@
 import styles from "../../../features/users/ui/UsersPage/Users.module.css";
 import React, {useState} from "react";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../../app/store/redux-store";
+import {setCurrentPage} from "../../../features/users/model/users-reducer";
 type PaginatorPropsType = {
     currentPage : number
     pageSize : number
     totalItemsCount: number
-    setCurrentPage: (page: number) => void
 
 }
-export const Paginator = ({currentPage, pageSize, totalItemsCount, setCurrentPage, ...props} : PaginatorPropsType) => {
+export const Paginator = ({currentPage, pageSize, totalItemsCount, ...props} : PaginatorPropsType) => {
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages: Array<number> = []
     const onPageChange = (changeDirection: '<' | '>') => <span className={styles.pageChangers} onClick={() => {
@@ -20,13 +22,14 @@ export const Paginator = ({currentPage, pageSize, totalItemsCount, setCurrentPag
 
     }
     const [portionCount, setPortionCount]= useState<number>(10)
+    const dispatch = useDispatch<AppDispatch>()
     return <div>
         {portionCount !== 10 && onPageChange('<')}
         {pages.map(page => {
             if (page <= portionCount && page > portionCount - 10) {
                 return (
                     <span key={page}
-                          onClick={() => setCurrentPage(page)}
+                          onClick={() => dispatch(setCurrentPage(page))}
                           className={currentPage === page ? `${styles.page} ${styles.selectedPage}` : `${styles.page}`}>{page}</span>
                 )
             }

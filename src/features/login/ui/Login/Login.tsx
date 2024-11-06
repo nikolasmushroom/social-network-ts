154827dd6977c5f257835ab10c0d144f8fdce06e
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-06 23:58:08 +0300

    Login.tsx was refactored to Functional component with hooks and without LoginContainer.tsx component which mapped state to props.

--- src/features/login/ui/Login/Login.tsx
+++ src/features/login/ui/Login/Login.tsx
@@ -2,15 +2,18 @@ import {Navigate} from "react-router-dom";
 import React, {useEffect} from "react";
 import classes from "./Login.module.css";
 import {LoginForm} from "./loginForm/LoginForm";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootReduxStateType} from "../../../../app/store/redux-store";
+import {getLogInfo} from "../../model/auth-reducer";
 
-export type LoginPropsType = {
-    login: (email: string, password: string, rememberMe: boolean, captcha :string) => void
-    isAuth: boolean
-    captchaUrl: string
-    error: string
-}
-export const Login = ({isAuth, login, error, captchaUrl}: LoginPropsType) => {
+export type LoginPropsType = {}
+export const Login = ({}: LoginPropsType) => {
+    const isAuth = useSelector((state : RootReduxStateType) => state.auth.isAuth)
+    const dispatch = useDispatch<AppDispatch>()
     useEffect(() => {
+        if(getLogInfo){
+            dispatch(getLogInfo())
+        }
     }, [isAuth])
     if (isAuth) {
         return <Navigate to={'/profile'}/>
@@ -18,7 +21,7 @@ export const Login = ({isAuth, login, error, captchaUrl}: LoginPropsType) => {
         return (
             <div className={classes.login}>
                 <h1>LOGIN</h1>
-                <LoginForm login={login} captchaUrl={captchaUrl} error={error}/>
+                <LoginForm/>
             </div>
         )
     }

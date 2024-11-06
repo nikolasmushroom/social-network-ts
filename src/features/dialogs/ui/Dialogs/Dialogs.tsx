a43a2800c760fa319d9e152c39bb97936a05e270
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-06 23:56:25 +0300

    Dialogs was refactored to Functional component with hooks and without DialogsContainer.tsx

--- src/features/dialogs/ui/Dialogs/Dialogs.tsx
+++ src/features/dialogs/ui/Dialogs/Dialogs.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import classes from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/Dialogitem";
 import Message from "./DialogMessage/Message";
 import {DialogsContainerType} from "../../../../app/store/store";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootReduxStateType} from "../../../../app/store/redux-store";
+import {sendMessage, updateNewMessage} from "../../model/dialogs-reducer";
+import {useNavigate} from "react-router-dom";
 
-export const Dialogs = ({
-                            dialogs,
-                            messages,
-                            newMessageText,
-                            updateNewMessage,
-                            sendMessage,
-                        }: DialogsContainerType) => {
+export const Dialogs = ({}: DialogsContainerType) => {
+    const dialogs = useSelector((state: RootReduxStateType) => state.dialogsPage.dialogs)
+    const messages = useSelector((state: RootReduxStateType) => state.dialogsPage.messages)
+    const newMessageText = useSelector((state: RootReduxStateType) => state.dialogsPage.newMessageText)
+    const isAuth = useSelector((state: RootReduxStateType) => state.auth.isAuth)
+    const navigate = useNavigate()
+    const dispatch = useDispatch<AppDispatch>()
+    useEffect(() => {
+        if (!isAuth) {
+            return navigate('/login')
+        }
+    }, [isAuth])
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
@@ -31,9 +40,9 @@ export const Dialogs = ({
                 </div>
                 <div>
                     <textarea onChange={(e) => {
-                        updateNewMessage(e.currentTarget.value)
+                        dispatch(updateNewMessage(e.currentTarget.value))
                     }} value={newMessageText}></textarea>
-                    <button onClick={sendMessage}>Send Message</button>
+                    <button onClick={() => dispatch(sendMessage)}>Send Message</button>
                 </div>
             </div>
         </div>

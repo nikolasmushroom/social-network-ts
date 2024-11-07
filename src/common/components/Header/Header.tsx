5d96da42a79a30ba0c6f67d2738d3d7ca5aefea1
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-07 23:14:06 +0300

    Header.tsx was refactored to get information about login and authinfication direct from state use useSelector and dispatch thunkCreator to change state and to make async operations.

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -3,17 +3,19 @@ import classes from './Header.module.css';
 import logo from '../../../asserts/images/logo.png'
 import {NavLink} from "react-router-dom";
 import {Button} from "../Button/Button";
-type HeaderPropsType = {
-    isAuth : boolean
-    login  : string
-    logout? : () => void
-}
-const Header = ({isAuth, login, logout} : HeaderPropsType) => {
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootReduxStateType} from "../../../app/store/redux-store";
+import {logout} from "../../../features/login/model/auth-reducer";
+type HeaderPropsType = {}
+const Header = ({} : HeaderPropsType) => {
+    const isAuth = useSelector((state : RootReduxStateType) => state.auth.isAuth)
+    const login = useSelector((state : RootReduxStateType) => state.auth.login)
+    const dispatch = useDispatch<AppDispatch>()
     return (
         <header className={classes.header}>
             <img src={logo} alt="logo"/>
             <div className={classes.loginBlock}>
-                {isAuth ? <div>{login} <Button onClick={logout}>Log out</Button> </div>: <NavLink to={'/login'}>Login</NavLink>}
+                {isAuth ? <div>{login} <Button onClick={() => dispatch(logout())}>Log out</Button> </div>: <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>
     )

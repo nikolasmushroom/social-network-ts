77c5ac22424853eff7dd7764940f03dbee3924e6
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-07 23:18:34 +0300

    Users.tsx was refactored to component which directly without UserContainer.tsx and geting data from state(using useSelector) and changing data(using useDispatch).

--- src/features/users/ui/UsersPage/Users.tsx
+++ src/features/users/ui/UsersPage/Users.tsx
@@ -1,35 +1,40 @@
 import {UserType} from "../../../../app/store/store";
 import styles from './Users.module.css'
-import React from "react";
+import React, {useEffect} from "react";
 import {Paginator} from "../../../../common/components/Paginator/Paginator";
 import {User} from "./User/User";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootReduxStateType} from "../../../../app/store/redux-store";
+import {requestUsers} from "../../model/users-reducer";
+import {Preloader} from "../../../../common/components/Preloader/Preloader";
 
 export type UsersPropsType = {
-    users: UserType[]
-    pageSize: number
-    totalUsersCount: number
-    currentPage: number
-    setCurrentPage: (page: number) => void
-    followingInProgress: Array<string>
-    changeFollowStatus: (u : UserType, followStatus : boolean) => void
 }
-export const Users = ({
-                          users,
-                          currentPage,
-                          totalUsersCount,
-                          pageSize,
-                          setCurrentPage,
-                          followingInProgress,
-                          changeFollowStatus,
-                      }: UsersPropsType) => {
-    return (
-        <div className={styles.users}>
-            <div>
-                {users.map(u => <User key={u.id} user={u} followingInProgress={followingInProgress} changeFollowStatus={changeFollowStatus}/>)}
-            </div>
 
-           <Paginator pageSize={pageSize} totalItemsCount={totalUsersCount} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
-        </div>
+export const Users = ({}: UsersPropsType) => {
+    const users = useSelector((state: RootReduxStateType) => state.usersPage.users)
+    const currentPage = useSelector((state: RootReduxStateType) => state.usersPage.currentPage)
+    const totalUsersCount = useSelector((state: RootReduxStateType) => state.usersPage.totalUsersCount)
+    const pageSize = useSelector((state: RootReduxStateType) => state.usersPage.pageSize)
+    const isFetching = useSelector((state: RootReduxStateType) => state.usersPage.isFetching)
+    const dispatch = useDispatch<AppDispatch>()
+    useEffect(() => {
+        dispatch(requestUsers(currentPage, pageSize))
+    }, [currentPage, pageSize])
+    return (
+        <>
+            {isFetching && <Preloader className={styles.loading}/>}
+
+            {!isFetching &&
+                <div className={styles.users}>
+                    <div>
+                        {users.map(u => <User key={u.id} user={u}/>)}
+                    </div>
+                    <Paginator pageSize={pageSize} totalItemsCount={totalUsersCount}
+                               currentPage={currentPage}/>
+                </div>
+            }
+        </>
     )
 }
 export default Users

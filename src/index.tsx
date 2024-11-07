40f2c11a840f96f751e857b8c4c10a73db7b9140
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-07 23:08:58 +0300

    In index.tsx was added Provider with store and BrowserRouter to create context under App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,25 @@ import './index.css';
 import './index.css';
 
 import {createRoot} from "react-dom/client";
-import {MainApp} from "./app/AppContainer";
+import React from "react";
+import {Provider} from "react-redux";
+import store from "./app/store/redux-store";
+import {BrowserRouter} from "react-router-dom";
+import App from "./app/App";
 
 
 const container = (document.getElementById('root'));
 if (container) {
     const root = createRoot(container)
     root.render(
-        <MainApp/>
+        <Provider store={store}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <App/>
+            </BrowserRouter>
+        </Provider>
     )
 } else {
     console.error("Element with id 'root' not found.");
 }
-
 
 

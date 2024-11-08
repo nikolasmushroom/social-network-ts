191f8ecc87830388fa7ac38a9e1cba01b6399078
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-08 22:50:17 +0300

    In profile-reducer.ts were added types for dispatch (AppDispatch) and were added response types.

--- src/features/profile/model/profile-reducer.ts
+++ src/features/profile/model/profile-reducer.ts
@@ -9,6 +9,7 @@ import {
 import {v1} from "uuid";
 import {usersAPI} from "../../users/api/usersAPI";
 import {profileAPI} from "../api/profileAPI";
+import {AppDispatch} from "../../../app/store/redux-store";
 
 export const ADD_POST = "ADD-POST"
 export const CHANGE_INPUT = "CHANGE-INPUT"
@@ -38,7 +39,7 @@ const initialState: ProfilePageType = {
         "lookingForAJob": false,
         "lookingForAJobDescription": '',
         "fullName": '',
-        "userId": 1,
+        "userId": 0,
         "photos": {
             "small": '',
             "large": ''
@@ -64,12 +65,11 @@ export const profileReducer = (state: ProfilePageType = initialState, action: Ac
             return {
                 ...state,
                 profile: {
-                    ...state.profile,
                     ...action.profile,
                     contacts: {
                         ...action.profile.contacts
                     }
-                }
+                },
             };
         case SET_STATUS:
             return {
@@ -124,23 +124,23 @@ export const setProfileError = (error: string) => ({
     type: "SET_PROFILE_ERROR",
     error: error
 }) as const
-export const getUserProfile = (userId: string, isAuth: boolean) => async (dispatch: any) => {
+export const getUserProfile = (userId: string, isAuth: boolean) => async (dispatch: AppDispatch)  => {
     if (isAuth) {
         const response = await usersAPI.getUserProfile(userId)
         dispatch(setUserProfileActionCreator(response.data))
     }
 }
-export const getUserStatus = (userId: string) => async (dispatch: any) => {
-    const response = await profileAPI.getUserStatus(userId)
+export const getUserStatus = (userId: string) => async (dispatch: AppDispatch) => {
+    const response = await profileAPI.getUserStatus(parseInt(userId))
     dispatch(setStatusActionCreator(response.data))
 }
-export const updateUserStatus = (status: string) => async (dispatch: any) => {
+export const updateUserStatus = (status: string) => async (dispatch: AppDispatch) => {
     const response = await profileAPI.updateUserStatus(status)
     if (response.data.resultCode === 0) {
         dispatch(setStatusActionCreator(status))
     }
 }
-export const updateUserProfile = (profile: ProfileType) => (dispatch: any) => {
+export const updateUserProfile = (profile: ProfileType) => (dispatch: AppDispatch) => {
     return profileAPI.updateUserProfileData(profile)
         .then((response) => {
             if (response.data.resultCode === 0) {
@@ -151,7 +151,7 @@ export const updateUserProfile = (profile: ProfileType) => (dispatch: any) => {
             }
         })
 }
-export const savePhoto = (file: string) => async (dispatch: any) => {
+export const savePhoto = (file: File) => async (dispatch: AppDispatch) => {
     const response = await profileAPI.savePhoto(file)
     if (response.data.resultCode === 0) {
         dispatch(savePhotoActionCreator(response.data.data.photos))
